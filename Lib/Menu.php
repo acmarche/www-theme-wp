adbc36ea5c6b4dd54ada8cd4de5271629d3db4fc
jfsenechal <jfsenechal@gmail.com>
2020-11-17 22:09:58 +0100

    cache

--- Lib/Menu.php
+++ Lib/Menu.php
@@ -5,6 +5,8 @@ namespace AcMarche\Theme\Lib;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use AcMarche\Common\Twig;
 use Twig\Environment;
+use Symfony\Component\Cache\Adapter\ApcuAdapter;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class Menu
 {
@@ -28,21 +30,45 @@ class Menu
     {
         switch_to_blog($id_site);
 
-        //todo cache
-        $menu = wp_get_nav_menu_object(self::MENU_NAME);
+        $cache = new ApcuAdapter(
 
-        $args = array(
-            'order'                  => 'ASC',
-            'orderby'                => 'menu_order',
-            'post_type'              => 'nav_menu_item',
-            'post_status'            => 'publish',
-            'output'                 => ARRAY_A,
-            'output_key'             => 'menu_order',
-            'nopaging'               => true,
-            'update_post_term_cache' => false,
+        // a string prefixed to the keys of the items stored in this cache
+            $namespace = '',
+
+            // the default lifetime (in seconds) for cache items that do not define their
+            // own lifetime, with a value 0 causing items to be stored indefinitely (i.e.
+            // until the APCu memory is cleared)
+            $defaultLifetime = 0,
+
+            // when set, all keys prefixed by $namespace can be invalidated by changing
+            // this $version string
+            $version = null
         );
 
-        return wp_get_nav_menu_items($menu, $args);
+        // The callable will only be executed on a cache miss.
+        $value = $cache->get(
+            'menu_cache_'.$id_site,
+            function (ItemInterface $item) {
+                $item->expiresAfter(3600);
+
+                $menu = wp_get_nav_menu_object(self::MENU_NAME);
+
+                $args = array(
+                    'order'                  => 'ASC',
+                    'orderby'                => 'menu_order',
+                    'post_type'              => 'nav_menu_item',
+                    'post_status'            => 'publish',
+                    'output'                 => ARRAY_A,
+                    'output_key'             => 'menu_order',
+                    'nopaging'               => true,
+                    'update_post_term_cache' => false,
+                );
+
+                return wp_get_nav_menu_items($menu, $args);
+            }
+        );
+
+        return $value;
     }
 
     public function renderAll()
