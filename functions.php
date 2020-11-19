bad64bb6daa2d08727d08e1fa9df5787352c89e1
jfsenechal <jfsenechal@gmail.com>
2020-11-19 15:20:10 +0100

    security

--- functions.php
+++ functions.php
@@ -10,3 +10,5 @@ add_action('after_setup_theme', [Setup::get_instance(), 'marchebe_setup']);
 add_action('wp_enqueue_scripts', [Setup::get_instance(), 'marchebe_scripts']);
 add_action('widgets_init', [Setup::get_instance(), 'marchebe_widgets_init']);
 add_action('pre_get_posts', [Setup::get_instance(), 'alterMainQuery']);
+
+require_once 'inc/_security.php';
