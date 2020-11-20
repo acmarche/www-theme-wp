fea6684da9532d6d967b0ef2c7eca1a4a23f4b04
jfsenechal <jfsenechal@gmail.com>
2020-11-20 14:58:44 +0100

    agenda, events

---
+++ category-agenda-des-manifestations.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AcMarche\Theme;
+
+use AcMarche\Common\Twig;
+use AcMarche\Hades\HadesWpRepository;
+
+get_header();
+
+$twig            = Twig::LoadTwig();
+$hadesRepository = new HadesWpRepository();
+$posts           = $hadesRepository->getEvents();
+
+$content = $twig->render(
+    'agenda/agenda.html.twig',
+    [
+        'posts' => $posts,
+    ]
+);
+echo $content;
+get_footer();
