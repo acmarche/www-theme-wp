dc645fdf7b8e3b743176d40c954b58788ca3ce41
jfsenechal <jfsenechal@gmail.com>
2020-11-20 15:58:40 +0100

    agenda, events

--- homepage.php
+++ homepage.php
@@ -2,11 +2,25 @@
 
 namespace AcMarche\Theme;
 
+use AcMarche\Common\Twig;
+use AcMarche\Common\WpRepository;
+use AcMarche\Hades\HadesWpRepository;
+
 /**
-  Template Name: Home-Page-Principal
+ * Template Name: Home-Page-Principal
  */
 get_header();
 
-get_template_part( 'template-parts/content/home/content-home' );
-
+$twig            = Twig::LoadTwig();
+$news            = WpRepository::getAllNews(6);
+$hadesRepository = new HadesWpRepository();
+$events          = $hadesRepository->getEvents(10);
+$content         = $twig->render(
+    'home/home_content.html.twig',
+    [
+        'news'   => $news,
+        'events' => $events,
+    ]
+);
+echo $content;
 get_footer();
