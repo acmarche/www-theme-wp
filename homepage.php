d18352fd9e80cf6bf6b97c71b7bed688ceae9760
jfsenechal <jfsenechal@gmail.com>
2020-11-24 10:44:16 +0100

    remove old hades

--- homepage.php
+++ homepage.php
@@ -4,7 +4,6 @@ namespace AcMarche\Theme;
 
 use AcMarche\Common\Twig;
 use AcMarche\Common\WpRepository;
-use AcMarche\Pivot\Repository\HadesWpRepository;
 use AcMarche\Pivot\Repository\PivotRemoteRepository;
 
 /**
@@ -12,20 +11,17 @@ use AcMarche\Pivot\Repository\PivotRemoteRepository;
  */
 get_header();
 
-$hadesRepository = new HadesWpRepository();
 $pivotRepository = new PivotRemoteRepository();
 $twig            = Twig::LoadTwig();
 
 $news   = WpRepository::getAllNews(6);
-$events = $hadesRepository->getEvents(10);
-$eventsPivot = $pivotRepository->getAllEvents();
+$events = $pivotRepository->getAllEvents();
 
 $content = $twig->render(
     'home/home_content.html.twig',
     [
         'news'   => $news,
         'events' => $events,
-        'eventsPivot' => $eventsPivot,
     ]
 );
 echo $content;
