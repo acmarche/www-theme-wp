cfcc0fbe5e52a0a8ef1a783fbdc60c9e62299048
jfsenechal <jfsenechal@gmail.com>
2020-11-27 14:14:33 +0100

    add get fiche by post

--- single-bottin_fiche.php
+++ single-bottin_fiche.php
@@ -4,6 +4,7 @@ namespace AcMarche\Theme;
 
 use AcMarche\Bottin\Bottin;
 use AcMarche\Bottin\Repository\BottinRepository;
+use AcMarche\Bottin\Repository\WpRepository;
 use AcMarche\Common\Twig;
 use AcMarche\Theme\Inc\Router;
 
@@ -13,12 +14,15 @@ global $wp_query;
 $twig             = Twig::LoadTwig();
 $bottinRepository = new BottinRepository();
 
-$slugFiche = $wp_query->get(Router::PARAM_BOTTIN);
-
-if ($slugFiche) {
-    $fiche = $bottinRepository->getFicheBySlug($slugFiche);
+if ($post = $wp_query->post) {
+    WpRepository::set_table_meta();
+    $idfiche = get_metadata(WpRepository::DATA_TYPE, $post->ID, 'id', true);
+    $fiche   = $bottinRepository->getFicheById($idfiche);
 } else {
-    $fiche = null;
+    $slugFiche = $wp_query->get(Router::PARAM_BOTTIN);
+    if ($slugFiche) {
+        $fiche = $bottinRepository->getFicheBySlug($slugFiche);
+    }
 }
 
 if ( ! $fiche) {
@@ -38,7 +42,6 @@ $logo          = $bottinRepository->getLogo($fiche->id);
 if ($logo) {
     unset($images[0]);
 }
-
 array_map(
     function ($category) {
         $category->url = '/bottin/category/'.$category->slug;
