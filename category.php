829bfa919dfb22b2bf19cc3329feb6b2a43a3645
jfsenechal <jfsenechal@gmail.com>
2020-12-01 17:14:41 +0100

    mix posts and fiche

--- category.php
+++ category.php
@@ -25,30 +25,33 @@ $children = get_categories($args);
 array_map(
     function ($category) {
         $category->permalink = get_category_link($category->term_id);
-        $category->id = $category->term_id;
+        $category->id        = $category->term_id;
     },
     $children
 );
 
-WpRepository::set_table_meta();
+$categoryBottinId = get_term_meta($cat_ID, \BottinCategoryMetaBox::KEY_NAME, true);
+if ($categoryBottinId) {
+    $fiches = $bottinRepository->getFichesByCategory($categoryBottinId);
+}
+
+$all = array_merge($posts, $fiches);
+
+$path = get_blog_details(get_current_blog_id())->path;
 
 array_map(
-    function ($post) use ($bottinRepository) {
-        if ($post->post_type == 'bottin_fiche') {
-            $idfiche = get_metadata(WpRepository::DATA_TYPE, $post->ID, 'id', true);
-            $fiche   = $bottinRepository->getFicheById($idfiche);
-            if ($fiche) {
-                $post->fiche     = $fiche;
-                $post->excerpt   = Bottin::getExcerpt($fiche);
-                $post->permalink = get_permalink($post->ID);
-                $post->permalink = '/bottin/fiche/'.$fiche->slug;
-            }
-        } else {
+    function ($post) use ($path) {
+        if ($post instanceof \WP_Post) {
             $post->excerpt   = $post->post_excerpt;
             $post->permalink = get_permalink($post->ID);
+        } else {
+            $post->fiche      = true;
+            $post->excerpt    = Bottin::getExcerpt($post);
+            $post->permalink  = $path.'bottin/fiche/'.$post->slug;
+            $post->post_title = $post->societe;
         }
     },
-    $posts
+    $all
 );
 
 $content = $twig->render(
@@ -57,7 +60,7 @@ $content = $twig->render(
         'title'       => $title,
         'description' => $description,
         'children'    => $children,
-        'posts'       => $posts,
+        'posts'       => $all,
     ]
 );
 echo $content;
