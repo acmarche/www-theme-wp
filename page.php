23390f84b5f80d7b6d71a75d3470834acb7e8a72
jfsenechal <jfsenechal@gmail.com>
2020-12-02 14:00:58 +0100

    fix page

--- page.php
+++ page.php
@@ -13,13 +13,22 @@ $categories  = get_the_category($post->ID);
 $post_ID     = $post->ID;
 $titre       = $post->post_title;
 $description = strip_tags($post->post_excerpt);
-$url         = get_site_url()."/?p=$post_ID";
-$content     = $twig->render(
+$url         = get_permalink($post->ID);
+$logo        = null;
+if (has_post_thumbnail()) {
+    $images = wp_get_attachment_image_src(get_post_thumbnail_id(), 'original');
+    if ($images) {
+        $logo = $images[0];
+    }
+}
+$content = $twig->render(
     'article/show.html.twig',
     [
-        'post'       => $post,
-        'url'        => $url,
-        'categories' => $categories,
+        'post' => $post,
+        'url'  => $url,
+        'tags' => $categories,
+        'logo' => $logo,
+        'nom'  => $post->post_title,
     ]
 );
 
