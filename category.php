8e21ea65938cd82227bdae73c409abb7a9a3fa2a
jfsenechal <jfsenechal@gmail.com>
2020-12-09 22:34:15 +0100

    test react without plugin

--- category.php
+++ category.php
@@ -4,13 +4,33 @@
 namespace AcMarche\Theme;
 
 use AcMarche\Common\TemplateRender;
+use AcMarche\Common\Twig;
+
+wp_enqueue_script(
+    'react-app',
+    get_template_directory_uri().'/../../mu-plugins/wordpressmarche-react-plugin/build/index.js',
+    array('wp-element'),
+    time(),
+    true
+);
 
 get_header();
 
 global $wp_query;
 
+$category_id = get_queried_object_id();
+$blodId      = get_current_blog_id();
+$site_slug   = get_blog_details($blodId)->path;
+$name        = get_bloginfo('name');
 //echo TemplateRender::renderCategory($wp_query);
-
-echo do_shortcode('[example_react_app]');
-
+$twig    = Twig::LoadTwig();
+$content = $twig->render(
+    'category/category.html.twig',
+    [
+        'title'       => $name,
+        'category_id' => $category_id,
+        'site_slug'   => $site_slug,
+    ]
+);
+echo $content;
 get_footer();
