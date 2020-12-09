b7f6f7b9052928d46305937ca13858f2777624b9
jfsenechal <jfsenechal@gmail.com>
2020-12-09 16:55:13 +0100

    extract logic pour react

--- category.php
+++ category.php
@@ -3,64 +3,14 @@
 
 namespace AcMarche\Theme;
 
-use AcMarche\Bottin\Bottin;
-use AcMarche\Bottin\Repository\BottinRepository;
-use AcMarche\Common\Twig;
-use AcMarche\Theme\Inc\Router;
+use AcMarche\Common\TemplateRender;
 
 get_header();
 
 global $wp_query;
-$twig             = Twig::LoadTwig();
-$bottinRepository = new BottinRepository();
 
-$cat_ID      = get_queried_object_id();
-$description = category_description($cat_ID);
-$title       = single_cat_title('', false);
+//echo TemplateRender::renderCategory($wp_query);
 
-$posts = $wp_query->get_posts();
+echo do_shortcode('[example_react_app]');
 
-$args     = ['parent' => $cat_ID, 'hide_empty' => false];
-$children = get_categories($args);
-array_map(
-    function ($category) {
-        $category->permalink = get_category_link($category->term_id);
-        $category->id        = $category->term_id;
-    },
-    $children
-);
-
-$fiches           = [];
-$categoryBottinId = get_term_meta($cat_ID, \BottinCategoryMetaBox::KEY_NAME, true);
-if ($categoryBottinId) {
-    $fiches = $bottinRepository->getFichesByCategory($categoryBottinId);
-}
-
-$all = array_merge($posts, $fiches);
-
-array_map(
-    function ($post) {
-        if ($post instanceof \WP_Post) {
-            $post->excerpt   = $post->post_excerpt;
-            $post->permalink = get_permalink($post->ID);
-        } else {
-            $post->fiche      = true;
-            $post->excerpt    = Bottin::getExcerpt($post);
-            $post->permalink  = Router::getUrlFicheBottin($post);
-            $post->post_title = $post->societe;
-        }
-    },
-    $all
-);
-
-$content = $twig->render(
-    'category/category.html.twig',
-    [
-        'title'       => $title,
-        'description' => $description,
-        'children'    => $children,
-        'posts'       => $all,
-    ]
-);
-echo $content;
 get_footer();
