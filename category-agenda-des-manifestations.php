f651e94ecb2f6a0ac3e5c94b55b341dac5bf323d
jfsenechal <jfsenechal@gmail.com>
2020-12-20 22:55:10 +0100

    init agenda

--- category-agenda-des-manifestations.php
+++ category-agenda-des-manifestations.php
@@ -10,9 +10,10 @@ get_header();
 $twig            = Twig::LoadTwig();
 $hadesRepository = new HadesRepository();
 $events          = $hadesRepository->getEvents();
+wp_enqueue_script('react-app', get_locale_stylesheet_uri().'/../assets/build/agenda.js', array('wp-element'), time(), true);//For production use wp_get_theme()->get('Version')
 
 $content = $twig->render(
-    'agenda/index.html.twig',
+    'agenda/index_react.html.twig',
     [
         'events' => $events,
     ]
