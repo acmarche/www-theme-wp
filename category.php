4328c5e4bf8e34f45e333361542f2394f6274031
jfsenechal <jfsenechal@gmail.com>
2020-12-30 15:10:38 +0100

    set react

--- category.php
+++ category.php
@@ -3,11 +3,45 @@
 
 namespace AcMarche\Theme;
 
+use AcMarche\Common\MarcheConst;
 use AcMarche\Common\TemplateRender;
+use AcMarche\Common\Twig;
 
 get_header();
 
-TemplateRender::renderCategory();
-//echo do_shortcode('[example_react_app]');
+//TemplateRender::renderCategory();
+
+$cat_ID      = get_queried_object_id();
+$category    = get_category($cat_ID);
+$description = $category->description;
+$title       = $category->name;
+$blodId      = get_current_blog_id();
+if ($blodId === 1) {
+    $siteSlug = 'Citoyen';
+    $path     = '';
+} else {
+    $siteSlug = $path = get_blog_details($blodId)->path;
+}
+
+$color = MarcheConst::COLORS[$blodId];
+wp_enqueue_script(
+    'react-app',
+    get_template_directory_uri().'/assets/js/build/category.js',
+    array('wp-element'),
+    time(),
+    true
+);//For production use wp_get_theme()->get('Version')
+
+Twig::rendPage(
+    'category/category_react.html.twig',
+    [
+        'title'   => $title,
+        'category' => $category,
+        'siteSlug' => $siteSlug,
+        'color'    => $color,
+        'path'     => $path,
+        'subTitle' => 'Tout',
+    ]
+);
 
 get_footer();
