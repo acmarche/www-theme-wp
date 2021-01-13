25685be26eafa648637a26953575e568581f1238
jfsenechal <jfsenechal@gmail.com>
2021-01-13 12:13:57 +0100

    template piscine

---
+++ pool.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Template Name: Piscine
+ */
+
+namespace AcMarche\Theme;
+
+use AcMarche\Common\Twig;
+
+get_header();
+global $post;
+
+$categories  = get_the_category($post->ID);
+$post_ID     = $post->ID;
+$titre       = $post->post_title;
+$description = strip_tags($post->post_excerpt);
+$url         = get_permalink($post->ID);
+$logo        = null;
+if (has_post_thumbnail()) {
+    $images = wp_get_attachment_image_src(get_post_thumbnail_id(), 'original');
+    if ($images) {
+        $logo = $images[0];
+    }
+}
+Twig::rendPage(
+    'pool/index.html.twig',
+    [
+        'title'   => $titre,
+        'post'    => $post,
+        'url'     => $url,
+        'tags'    => $categories,
+        'logo'    => $logo,
+        'content' => $post->post_content,
+    ]
+);
+get_footer();
