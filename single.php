e450a931aa1bc5e2dfbfb5275f1ffbb04d0fbbb5
jfsenechal <jfsenechal@gmail.com>
2021-01-27 11:47:07 +0100

    recommandations

--- single.php
+++ single.php
@@ -4,13 +4,14 @@
 namespace AcMarche\Theme;
 
 use AcMarche\Common\Twig;
+use AcMarche\Common\WpRepository;
 use AcMarche\Theme\Inc\Theme;
 
 get_header();
 global $post;
 
 $tags = get_the_category($post->ID);
-//get_the_posts_pagination();
+
 $image = null;
 if (has_post_thumbnail()) {
     $images = wp_get_attachment_image_src(get_post_thumbnail_id(), 'original');
@@ -19,22 +20,30 @@ if (has_post_thumbnail()) {
     }
 }
 
+$categorySlug = get_query_var('category_name');
+$category     = WpRepository::getCategoryBySlug($categorySlug);
+$urlBack      = get_category_link($category);
+
 $blodId = get_current_blog_id();
 
 $path     = Theme::getPathBlog($blodId);
 $blogName = Theme::getTitleBlog($blodId);
 $color    = Theme::getColorBlog($blodId);
 
+$relations = WpRepository::getRelations($post->ID, $tags);
+
 Twig::rendPage(
     'article/show.html.twig',
     [
-        'post'     => $post,
-        'tags'     => $tags,
-        'image'    => $image,
-        'title'    => $post->post_title,
-        'blogName' => $blogName,
-        'color'    => $color,
-        'path'     => $path,
+        'post'      => $post,
+        'tags'      => $tags,
+        'image'     => $image,
+        'title'     => $post->post_title,
+        'blogName'  => $blogName,
+        'color'     => $color,
+        'path'      => $path,
+        'relations' => $relations,
+        'url_back'  => $urlBack,
     ]
 );
 get_footer();
