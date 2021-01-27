f23a6dd6bb1713d7c92daa73c70dceac3f151e6d
jfsenechal <jfsenechal@gmail.com>
2021-01-27 11:46:01 +0100

    fix vars

--- 404.php
+++ 404.php
@@ -2,16 +2,20 @@
 
 namespace AcMarche\Theme;
 
+use AcMarche\Common\MarcheConst;
 use AcMarche\Common\Twig;
+use AcMarche\Theme\Inc\Theme;
 
 get_header();
 
 Twig::rendPage(
     'errors/404.html.twig',
     [
-        'title'   => 'Page non trouvée',
-        'tags'    => [],
-
+        'title'     => 'Page non trouvée',
+        'tags'      => [],
+        'color'     => Theme::getColorBlog(1),
+        'blogName'  => Theme::getTitleBlog(1),
+        'relations' => [],
     ]
 );
 get_footer();
