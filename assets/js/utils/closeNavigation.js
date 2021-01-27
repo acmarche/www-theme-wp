7831522c8447389956ba5541b182ab233741ef42
jfsenechal <jfsenechal@gmail.com>
2021-01-27 20:15:04 +0100

    close actions

---
+++ assets/js/utils/closeNavigation.js
@@ -0,0 +1,76 @@
+window.addEventListener( 'load', () => {
+
+    /**
+     * Fermeture ecran de recherche
+     */
+    const btnCloseSearch = document.getElementById( 'btn-close-search' );
+    const searchScreen = document.querySelector( '.searchScreen' );
+
+    btnCloseSearch.addEventListener( 'click', () => {
+        searchScreen.style.display = 'none';
+    });
+
+    /**
+     * Fermeture box alert
+     */
+    const btnCloseAlert = document.getElementById( 'btn-close-alert' );
+    const alertMessage = document.querySelector( '.object-alert' );
+
+    btnCloseAlert.addEventListener( 'click', () => {
+        alertMessage.style.display = 'none';
+    });
+
+    /**
+     * Fermeture main nav
+     */
+    const btnOpenMainNav = document.getElementById( 'btn-open-main-nav' );
+    const mainNav = document.getElementById( 'main-nav' );
+
+    btnOpenMainNav.addEventListener( 'click', () => {
+        mainNav.classList.add( 'toggle-opened' );
+        mainNav.classList.add( 'd-block' );
+    });
+
+    /**
+     * Fermeture main nav
+     */
+    const btnCloseMainNav = document.getElementById( 'btn-close-main-nav' );
+
+    btnCloseMainNav.addEventListener( 'click', () => {
+        mainNav.classList.add( 'd-none' );
+        mainNav.classList.remove( 'toggle-opened' );
+        mainNav.classList.remove( 'd-block' );
+    });
+
+    /**
+     * Fermeture sub nav
+     */
+    const btnCloseSubNav = document.querySelector( '.btn-close-sub-nav' );
+    const subNav = document.querySelector( '.sub-nav' );
+
+    btnCloseSubNav.addEventListener( 'click', () => {
+        console.log( 'clicked btn sub ' );
+        console.log( subNav );
+        subNav.classList.remove( 'toggle-opened' );
+    });
+});
+
+//La partie ci-dessus est responsable de la fermeture de la navigation principale
+//La classe toggle-opened est presente par defaut et provoque l'ouverture de la page
+//Enlever la classe manuellement ferme la page
+//clicker sur la croix pour enlever la classe fonctionne correctement mais ne ferme pas la navigation
+
+// TEST -----------------------------------------------------
+// let closeSecondNav = document.querySelectorAll(".closeSecondNav");
+// let secondNav = document.querySelectorAll(".secondNav");
+
+// closeSecondNav.forEach((elem) => {
+//   elem.addEventListener("click", () => {
+//     console.log("clicked");
+//     secondNav.forEach((el) => {
+//       el.style.display = "none";
+//     });
+//   });
+// });
+
+// TEST -----------------------------------------------------
