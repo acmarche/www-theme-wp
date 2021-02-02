09709d883b65dae68e685be619335186952569ab
jfsenechal <jfsenechal@gmail.com>
2021-02-02 15:06:13 +0100

    remove if react

--- category-agenda-des-manifestations.php
+++ category-agenda-des-manifestations.php
@@ -4,35 +4,25 @@ namespace AcMarche\Theme;
 
 use AcMarche\Common\Twig;
 use AcMarche\Pivot\Repository\HadesRepository;
-use AcMarche\Theme\Inc\SettingsPage;
 
 get_header();
 
 $hadesRepository = new HadesRepository();
 $events          = $hadesRepository->getEvents();
-$react           = SettingsPage::isReactActivate();
 
-if ($react) {
-    wp_enqueue_script(
-        'react-app',
-        get_template_directory_uri().'/assets/js/build/agenda.js',
-        array('wp-element'),
-        time(),
-        true
-    );//For production use wp_get_theme()->get('Version')
+wp_enqueue_script(
+    'react-app',
+    get_template_directory_uri().'/assets/js/build/agenda.js',
+    array('wp-element'),
+    time(),
+    true
+);//For production use wp_get_theme()->get('Version')
 
-    Twig::rendPage(
-        'agenda/index_react.html.twig',
-        [
-            'events' => $events,
-        ]
-    );
-} else {
-    Twig::rendPage(
-        'agenda/index.html.twig',
-        [
-            'events' => $events,
-        ]
-    );
-}
+Twig::rendPage(
+    'agenda/index.html.twig',
+    [
+        'events' => $events,
+    ]
+);
+
 get_footer();
