2a85c9fba7f3703ac44efdaae305c337c60d21dd
jfsenechal <jfsenechal@gmail.com>
2021-02-10 13:51:06 +0100

    bug posts, cleaning

--- Inc/Filter.php
+++ Inc/Filter.php
@@ -8,8 +8,25 @@ class Filter
 {
     public function __construct()
     {
+        //add_filter('get_the_archive_title', [Setup::get_instance(), 'removeCategoryPrefixTitle']);
         // Stop WP adding extra <p> </p> to your pages' content
         remove_filter('the_content', 'wpautop');
         remove_filter('the_excerpt', 'wpautop');
+    }
+
+    /**
+     * Remove word "Category"
+     *
+     * @param $title
+     *
+     * @return string|void
+     */
+    function removeCategoryPrefixTitle($title)
+    {
+        if (is_category()) {
+            $title = single_cat_title('', false);
+        }
+
+        return $title;
     }
 }
