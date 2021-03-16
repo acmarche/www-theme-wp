2e0188dbdf65afa23fb295833bb67b79816ca14d
jfsenechal <jfsenechal@gmail.com>
2021-03-16 15:03:22 +0100

    homepage eco

--- homepage-eco.php
+++ homepage-eco.php
@@ -2,33 +2,21 @@
 
 namespace AcMarche\Theme;
 
-use AcMarche\Common\Mailer;
-use AcMarche\Theme\Inc\RouterMarche;
 use AcMarche\Theme\Lib\Twig;
 use AcMarche\Theme\Lib\WpRepository;
-use AcMarche\Pivot\Repository\HadesRepository;
 
 /**
  * Template Name: Home-Page-Eco
  */
 get_header();
+$wpRepository = new WpRepository();
 
-$hadesRepository = new HadesRepository();
-
-$news = WpRepository::getAllNews(6);
-try {
-    $events = $hadesRepository->getEvents();
-    RouterMarche::setRouteEvents($events);
-} catch (\Exception $exception) {
-    $events = [];
-    Mailer::sendError("Erreur de chargement de l'agenda", $exception->getMessage());
-}
+$news   = $wpRepository->getPostsByCategory(258, get_current_blog_id());
 
 Twig::rendPage(
-    'sport/index.html.twig',
+    'eco/index.html.twig',
     [
-        'actus'        => $news,
-        'events'       => $events,
+        'actus'  => $news,
     ]
 );
 
