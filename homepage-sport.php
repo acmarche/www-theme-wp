c9e2c5c421cbcc6958c8bae3c9007844c85ac92b
jfsenechal <jfsenechal@gmail.com>
2021-03-16 13:55:11 +0100

    homepage sport eco

---
+++ homepage-sport.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AcMarche\Theme;
+
+use AcMarche\Common\Mailer;
+use AcMarche\Theme\Inc\RouterMarche;
+use AcMarche\Theme\Lib\Twig;
+use AcMarche\Theme\Lib\WpRepository;
+use AcMarche\Pivot\Repository\HadesRepository;
+
+/**
+ * Template Name: Home-Page-Sport
+ */
+get_header();
+
+$hadesRepository = new HadesRepository();
+
+$news = WpRepository::getAllNews(6);
+try {
+    $events = $hadesRepository->getEvents();
+    RouterMarche::setRouteEvents($events);
+} catch (\Exception $exception) {
+    $events = [];
+    Mailer::sendError("Erreur de chargement de l'agenda", $exception->getMessage());
+}
+
+
+Twig::rendPage(
+    'sport/index.html.twig',
+    [
+        'actus'        => $news,
+        'events'       => $events,
+    ]
+);
+
+get_footer();
