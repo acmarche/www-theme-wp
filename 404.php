1c68236f8c9ac91a3125cdb5f12ebe0eec3c0916
jfsenechal <jfsenechal@gmail.com>
2021-03-18 11:02:15 +0100

    clean

--- 404.php
+++ 404.php
@@ -2,37 +2,10 @@
 
 namespace AcMarche\Theme;
 
-use AcMarche\Theme\Inc\Carto;
-use AcMarche\Theme\Lib\KmlParser;
 use AcMarche\Theme\Lib\Twig;
 use AcMarche\Theme\Inc\Theme;
 
 get_header();
-$carto  = new Carto();
-$kml    = $carto->loadKml('statues');
-$kmlParser = new KmlParser();
-$domdoc = $kmlParser->parse($kml);
-$places = $kmlParser->getPlacesMark();
-dump($places);
-
-foreach ($places as $place) {
-    // dump($offre);
-    if ($place instanceof \DOMElement) {
-        $nodeName    = $place->nodeName;
-        $placeName       = $kmlParser->getValueByTagName($place,'name');
-        $point       = $kmlParser->getElementsByTagName($place,'Point');
-        $description = $kmlParser->getValueByTagName($place,'description');
-        dump($nodeName);
-        dump($placeName);
-        dump($point);
-        dump($description);
-        $coordinates = $kmlParser->getValueByTagName($point,'coordinates');
-        dump($coordinates);
-        foreach ($place->childNodes as $child) {
-            //      dump($child);
-        }
-    }
-}
 
 Twig::rendPage(
     'errors/404.html.twig',
