fa354acf72dbc3cc64603ae3dd81035ae8b92d27
jfsenechal <jfsenechal@gmail.com>
2021-06-08 14:14:18 +0200

    add mailchimp

--- Lib/MailChimp.php
+++ Lib/MailChimp.php
@@ -30,14 +30,30 @@ class MailChimp
     public function getCampaings(): array
     {
         $data = [];
-        $list = $this->apiClient->campaigns->list();
+        $list = $this->apiClient->campaigns->list(
+            null,
+            null,
+            15,
+            0,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            'send_time',
+            'DESC'
+        );
         $i    = 0;
         foreach ($list->campaigns as $campaing) {
-            $data[$i]['id']          = $campaing->id;
+            $data[$i]['id'] = $campaing->id;
             $data[$i]['archive_url'] = $campaing->archive_url;
-            $data[$i]['send_time']   = $campaing->send_time; //2021-05-12T15:10:19+00:00
-            $settings                = $campaing->settings;
-            $data[$i]['title']       = $settings->title;
+            $data[$i]['send_time'] = $campaing->send_time; //2021-05-12T15:10:19+00:00
+            $settings = $campaing->settings;
+            $data[$i]['title'] = $settings->title;
             $i++;
         }
 
