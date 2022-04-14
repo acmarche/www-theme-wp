78a13442064ad6ad8cdf2f432d25ae5883d34bd6
jfsenechal <jfsenechal@gmail.com>
2022-04-14 17:02:02 +0200

    switch to pivot

--- category-agenda-des-manifestations.php
+++ category-agenda-des-manifestations.php
@@ -6,30 +6,23 @@ use AcMarche\Pivot\DependencyInjection\Kernel;
 use AcMarche\Pivot\Repository\PivotRepository;
 use AcMarche\Theme\Inc\RouterMarche;
 use AcMarche\Theme\Lib\Twig;
-use Psr\Cache\InvalidArgumentException;
 
 get_header();
-
-$kernel = new Kernel('dev', true);
-$kernel->boot();
-$container = $kernel->getContainer();
-
-$loader = $container->get('dotenv');
-$loader->loadEnv('.env');
-
-/**
- * @var PivotRepository $pivot
- */
-$pivot = $container->get('pivotRepository');
-$codeCgt = "EVT-A1-0016-2D6U";
-$data = $pivot->offreByCgt($codeCgt, date('Y-m-d H:m:s'));
-dd($data->getOffre()->codeCgt);
-
-$events          = [];
 try {
+    $kernel = new Kernel('dev', true);
+    $kernel->boot();
+    $container = $kernel->getContainer();
+
+    $loader = $container->get('dotenv');
+    $loader->loadEnv('.env');
+    /**
+     * @var PivotRepository $pivotRepository
+     */
+    $pivotRepository = $container->get('pivotRepository');
     $events = $pivotRepository->getEvents();
+
     RouterMarche::setRouteEvents($events);
-} catch (InvalidArgumentException $e) {
+} catch (\Exception $e) {
     Twig::rendPage(
         'errors/500.html.twig',
         [
@@ -40,14 +33,6 @@ try {
 
     return;
 }
-
-wp_enqueue_script(
-    'react-app',
-    get_template_directory_uri().'/assets/js/build/agenda.js',
-    array('wp-element'),
-    wp_get_theme()->get('Version'),
-    true
-);
 
 Twig::rendPage(
     'agenda/index.html.twig',
