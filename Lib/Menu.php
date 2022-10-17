8e4a6f11a08d51e4dc9baf64e5d0fe1eebffda3e
jfsenechal <jfsenechal@gmail.com>
2022-10-17 22:13:54 +0200

    sort

--- Lib/Menu.php
+++ Lib/Menu.php
@@ -9,7 +9,7 @@ use Symfony\Contracts\Cache\CacheInterface;
 class Menu
 {
     const MENU_NAME = 'top-menu';
-    const MENU_CACHE_NAME = 'menu_all';
+    const MENU_CACHE_NAME = 'menu_all2';
 
     private CacheInterface $cache;
 
@@ -60,6 +60,33 @@ class Menu
                 switch_to_blog($blog);
 
                 return $this->sortByName($data);
+            }
+        );
+    }
+
+    public function getAllItems2(): array
+    {
+        return $this->cache->get(
+            self::MENU_CACHE_NAME.time(),
+            function (): array {
+                $blog = get_current_blog_id();
+                $data = [];
+                foreach (Theme::SITES as $idSite => $site) {
+                    if (in_array($idSite, [8, 12, 13])) {
+                        continue;
+                    }
+                    $data[$idSite]['name'] = ucfirst($site);
+                    if ($idSite == 14) {
+                        $data[$idSite]['name'] = 'Enfance-Jeunesse';
+                    }
+                    $data[$idSite]['blogid']     = $idSite;
+                    $data[$idSite]['colorhover'] = 'hover:text-'.$site;
+                    $data[$idSite]['color']      = 'text-'.$site;
+                    $data[$idSite]['items']      = $this->getItems($idSite);
+                }
+                switch_to_blog($blog);
+
+                return $data;
             }
         );
     }
