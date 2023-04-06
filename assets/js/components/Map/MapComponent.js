87eab4b40ef4460a5245ff4d4ac848858fcd8e6e
jfsenechal <jfsenechal@gmail.com>
2023-04-06 09:50:16 +0200

    zoom ko

--- assets/js/components/Map/MapComponent.js
+++ assets/js/components/Map/MapComponent.js
@@ -1,11 +1,12 @@
-import {MapContainer, TileLayer, Marker, Tooltip} from 'react-leaflet';
+import {MapContainer, TileLayer, Marker, Tooltip, useMap} from 'react-leaflet';
 import ReactLeafletKml from 'react-leaflet-kml';
 import ControlButtons from './ControlButtons';
 import PopupBottin from './popup/PopupBottin';
 import PopupKml from './popup/PopupKml';
 
 const {
-    useState
+    useState,
+    useEffect
 } = wp.element;
 
 function MapComponent({
@@ -14,15 +15,27 @@ function MapComponent({
                       }) {
 
     const [map, setMap] = useState(null);
+    const [zoomLevel, setZoomLevel] = useState(13);
 
     const handleClick = (object) => {
         //do nothing
     };
 
     function handleCreated(map) {
-        console.log('created')
+        console.log('created' + zoomLevel)
         setMap(map);
+        setZoomLevel(5)
+
+        console.log('sleeped' + zoomLevel)
     }
+
+    useEffect((zoomLevel) => {
+        // Mettre à jour le titre du document en utilisant l'API du navigateur
+        document.title = `Vous avez cliqué ${zoomLevel} fois`;
+        console.log('change zoom')
+        if (map != null)
+            map.zoom = zoomLevel
+    });
 
     return (
         <>
@@ -32,15 +45,16 @@ function MapComponent({
                 style={{width: 100 + '%'}}>
                 <MapContainer
                     id={'leaflet-container'}
+                    ref={}
                     style={{
                         width: '100%',
                         height: '700px'
                     }}
                     whenReady={handleCreated}
                     center={[50.22799745011792, 5.34405188915553]}
-                    zoom={13}
-                    zoomControl={true}
-                    scrollWheelZoom={true}
+                    zoom={zoomLevel}
+                    zoomControl={false}
+                    scrollWheelZoom={false}
                 >
                     <TileLayer
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
