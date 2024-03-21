6a64468c429e3e6d402a164c5ed01e2147dcfb14
jfsenechal <jfsenechal@gmail.com>
2024-03-21 08:42:14 +0100

    remove mail

--- index.php
+++ index.php
@@ -2,26 +2,21 @@
 
 namespace AcMarche\Theme;
 
-use AcMarche\Common\Mailer;
-use AcMarche\Theme\Lib\Twig;
 use AcMarche\Theme\Inc\Theme;
-use Symfony\Component\HttpFoundation\Request;
+use AcMarche\Theme\Lib\Twig;
 
 get_header();
 
 Twig::rendPage(
     'errors/500.html.twig',
     [
-        'message'   => 'Page vide',
-        'title'     => 'Error 500',
-        'tags'      => [],
-        'color'     => Theme::getColorBlog(1),
-        'blogName'  => Theme::getTitleBlog(1),
+        'message' => 'Page vide',
+        'title' => 'Error 500',
+        'tags' => [],
+        'color' => Theme::getColorBlog(1),
+        'blogName' => Theme::getTitleBlog(1),
         'relations' => [],
     ]
 );
 
-$request = Request::createFromGlobals();
-$url = $request->getUri();
-Mailer::sendError("Error page index.php", "url: $url");
 get_footer();
